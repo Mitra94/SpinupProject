43542a0891049f0c3627598699671b35d0dcb38d
Mitra94 <melideo.1595766@studenti.uniroma1.it>
2016-07-18 16:58:31 +0200

    Update rate.rb

--- spec/factories/rate.rb
+++ spec/factories/rate.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :rate do |f|
-    f.rater_id { FactoryGirl.create(:user).id  }
-    f.rateable_id { FactoryGirl.create(:app).id  }
-    f.rateable_type "App"
+    f.rater_id "1"
+    f.rateable_id "1"
+    f.stars "4"
   end
 end
