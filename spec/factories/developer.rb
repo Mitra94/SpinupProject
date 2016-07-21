a9856e8199f2338b424ac8a20272ae26688eab6b
Mitra94 <melideo.1595766@studenti.uniroma1.it>
2016-07-21 16:27:56 +0200

    Update developer.rb

--- spec/factories/developer.rb
+++ spec/factories/developer.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :developer do |f|
-    f.name { Faker::Internet.user_name(3) }
+    f.name { Faker::Internet.user_name(3) } # The name will be 3 characters or more
     f.surname { Faker::Name.last_name }
     f.email { Faker::Internet.email }
     f.city { Faker::Address.city }
